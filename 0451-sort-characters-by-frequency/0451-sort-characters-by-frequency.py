56d3619b7f0a1bea3b0072698630a1b29f99eab2
V. Manusri <velpula.manusri06@gmail.com>
2025-11-19 19:31:33 +0530

    Time: 11 ms (50.64%), Space: 18.7 MB (99.1%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,20 +1,41 @@
+import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
-        n = len(s)
+
         freq = {}
         for i in s:
             if i not in freq:
                 freq[i] = 0
             freq[i]+=1
-
-        bucket = [[] for _ in range(n+1)]
-
-        for ch,count in freq.items():
-            bucket[count].append(ch)
-
-        s = []
         
-        for i in range(n,0,-1):
-            for j in bucket[i]:
-                s.append(j*i)
-        return "".join(s)
+        q = []
+        for ch,count in freq.items():
+            heapq.heappush(q,(-count,ch))
+
+        result = []
+        while q:
+            count,ch = heapq.heappop(q)
+            result.append(ch*(-count))
+
+        return "".join(result)
+
+
+
+"""  n = len(s)
+freq = {}
+for i in s:
+    if i not in freq:
+        freq[i] = 0
+    freq[i]+=1
+
+bucket = [[] for _ in range(n+1)]
+
+for ch,count in freq.items():
+    bucket[count].append(ch)
+
+s = []
+
+for i in range(n,0,-1):
+    for j in bucket[i]:
+        s.append(j*i)
+return "".join(s) """
